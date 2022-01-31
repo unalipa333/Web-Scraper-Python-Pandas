bd221edce7339344e05b8eb730d75d34e9abb19c
Lyndon (Lin) Parrilla <linparrilla@gmail.com>
2022-01-30 17:44:44 -0800

    basic scrape
    requests and then requests with using bs4

---
+++ example0/ex0.py
@@ -0,0 +1,17 @@
+import requests
+
+
+payload = {'page':2, 'count': 25}
+r = requests.get('https://httpbin.org/get', params=payload)
+
+#print(r)
+
+#print(r.status_code)
+#print(r.ok)
+
+#print(dir(r))
+
+print(r.text)
+
+
+# https://www.youtube.com/watch?v=tb8gHvYlCFs
